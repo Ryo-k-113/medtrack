cd0a533c4b05a108954b2950629db667fe0a0e96
Ryo-k-113 <r.kameda.113@gmail.com>
2026-01-18 12:15:59 +0900

    fix: 新規登録をサーバーサイドに変更

--- src/lib/supabase-auth/auth.ts
+++ src/lib/supabase-auth/auth.ts
@@ -6,6 +6,34 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from 'next/cache'
 import { FormData } from '@/app/schemas/authSchema';
 
+//新規登録
+export const signupHandler = async(formData: FormData ) => { 
+  const supabase = await createClient()
+
+  const { error } = await supabase.auth.signUp({
+      email: formData.email,
+      password: formData.password,      
+      options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/login`,
+      },
+  })
+
+  if (error) {
+    if (error.message.includes("already")) {
+      return {
+          success: false,
+          message: "すでに登録されているメールアドレスです",
+        };
+    }
+  
+    return {
+      success: false,
+      message: "登録に失敗しました",
+    };
+  }
+
+  return { success: true };
+}
 
 
 // Googleログイン
