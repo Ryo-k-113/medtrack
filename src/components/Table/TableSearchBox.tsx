c1e5d9c319db83da4b37d1c940672e23f1561fd8
Ryo-k-113 <r.kameda.113@gmail.com>
2026-03-30 01:14:01 +0900

    fix: TableSearchBoxのディレクトリを変更

--- src/components/Table/TableSearchBox.tsx
+++ src/components/Table/TableSearchBox.tsx
@@ -14,7 +14,7 @@ type TableSearchBoxProps = {
 export const TableSearchBox = ({ value, onChange, placeholder }: TableSearchBoxProps) => {
   return (
     <div className="relative w-full max-w-sm">
-      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-foreground" />
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-foreground/50 " />
       <Input
         type="text"
         placeholder={placeholder}
