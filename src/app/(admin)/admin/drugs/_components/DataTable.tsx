15f010af09ed7f141fef1b7e9c0f1444a926f1fa
Ryo-k-113 <r.kameda.113@gmail.com>
2026-04-04 17:53:37 +0900

    feat: Tableコンポーネントを作成

---
+++ src/app/(admin)/admin/drugs/_components/DataTable.tsx
@@ -0,0 +1,79 @@
+"use client"
+import * as React from "react"
+import { useState } from "react"
+import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { TableSearchBox } from "@/components/TableSearchBox/TableSearchBox"
+import { Button } from "@/components/ui/button"
+
+type DataTableProps<TData, TValue> = {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+};
+
+export const DataTable = <TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) => {
+  const [globalFilter, setGlobalFilter] = useState("")
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    state: { globalFilter },
+    initialState: { pagination: { pageSize: 50 } }, // 50件ごとの表示設定
+  })
+ console.log(data)
+  return (
+    <div>
+      {/* 検索ボックス  */}
+      <div className="py-6">
+        <TableSearchBox
+          value={globalFilter ?? ""}
+          onChange={(value) => setGlobalFilter(String(value))}
+          placeholder="医薬品名で検索..."
+        />
+      </div>
+      {/* テーブル */}
+      <div className="rounded-md border overflow-hidden">
+        <Table>
+          <TableHeader className="bg-primary hover:bg-primary  sticky top-0 z-10">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} 
+              >
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} colSpan={header.colSpan} className="bg-primary text-primary-foreground font-bold">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+              ))}
+              </TableRow>
+            ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">データがありません</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      {/* ページネーション */}
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>前へ</Button>
+        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>次へ</Button>
+      </div>
+    </div>  
+  )
+}
