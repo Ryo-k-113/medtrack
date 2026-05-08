c3fb9d177877bb1d0fc4e739c823130d83513624
Ryo-k-113 <r.kameda.113@gmail.com>
2026-05-08 16:15:18 +0900

    chore: 不要な単語の削除

--- src/app/(admin)/admin/drugs/_components/FormInput.tsx
+++ src/app/(admin)/admin/drugs/_components/FormInput.tsx
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input"
 import { Field, FieldError, FieldLabel, FieldDescription} from "@/components/ui/field"
 import { cn } from "@/lib/utils";
 
-cn
 
 type FormInputProps ={
   name: string;
