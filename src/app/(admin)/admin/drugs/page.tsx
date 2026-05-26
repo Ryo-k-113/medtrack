f7616446c1a1b1f1543eee9d90a30c898820ed36
Ryo-k-113 <r.kameda.113@gmail.com>
2026-05-26 15:17:11 +0900

    feat:新規登録ボタンを追加

--- src/app/(admin)/admin/drugs/page.tsx
+++ src/app/(admin)/admin/drugs/page.tsx
@@ -1,14 +1,12 @@
 "use client"
 import * as React from "react"
 import { DataTable } from "@/components/Table/DataTable"
-// import { DrugPackageUnit } from "./_types/DrugPackageUnit"
 import { drugsColumns } from "./_components/drugsColumns"
 import { useDataFetch } from "@/hooks/useDataFetch"
 import  {  GetPublishedPackageUnitsResponse  } from '@/types/admin/drug';
-
-// type DrugsApiResponse = {
-//   packageUnits: DrugPackageUnit[];
-// };
+import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+import { Plus } from "lucide-react"
 
 export default function AdminDrugsPage() {
   //製品と製品包装情報を取得
@@ -21,19 +19,32 @@ export default function AdminDrugsPage() {
 
   return (
     <>
+      {/* タイトル */}
       <div className="border-b-2">
         <h2 className="pb-2 text-xl font-bold text-foreground">
           医薬品一覧
         </h2>
       </div>
-      <div className="p-4">
-        {isLoading ? (
-        <div className="text-center py-10">データを読み込み中...</div>
-        ) : (
-        <DataTable columns={drugsColumns} data={data} />
-        )}
+      
+      {/* 医薬品一覧のテーブル */}
+      {isLoading ? (
+      <div className="text-center py-10">データを読み込み中...</div>
+      ) : (
+      <div className="px-2 py-4">
+        <DataTable 
+          columns={drugsColumns} 
+          data={data} 
+          headerAction={ 
+            <Button variant="default" className="font-bold px-6" asChild>
+              <Link href="/admin/drugs/new">
+              <Plus className="h-4 w-4" />
+                新規登録
+              </Link>
+            </Button>
+          }
+        />
       </div>
-    </>
-    
+      )}
+    </> 
   ) 
 }
