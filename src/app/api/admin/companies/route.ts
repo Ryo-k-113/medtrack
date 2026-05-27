27c02071df634f3db7473292016be4ebb7254471
Ryo-k-113 <r.kameda.113@gmail.com>
2026-05-27 18:44:01 +0900

    feat: APIレスポンスに型定義を追加

--- src/app/api/admin/companies/route.ts
+++ src/app/api/admin/companies/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-
+import type { CompanyResponse } from "@/types/admin/drug"
 
 export const GET = async () => {
   try {
@@ -12,7 +12,7 @@ export const GET = async () => {
       orderBy: { name: "asc" },
     })
 
-    return NextResponse.json({ companies }, { status: 200 })
+    return NextResponse.json<CompanyResponse>({ companies }, { status: 200 })
   } catch (error) {
     if (error instanceof Error)
       return NextResponse.json({ message: error.message }, { status: 400 })
