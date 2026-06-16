d50f279f0518b1be8acb2ac3279ea69d196f9066
Ryo-k-113 <r.kameda.113@gmail.com>
2026-06-16 19:16:17 +0900

    feat: 告示種別の定数ANNOUNCE_TYPE_OPTIONSを追加

--- src/app/(admin)/admin/_constants/drug.ts
+++ src/app/(admin)/admin/_constants/drug.ts
@@ -1,4 +1,4 @@
-import { ProductType, CurrentShippingStatus } from "@prisma/client"
+import { ProductType, CurrentShippingStatus, AnnounceType } from "@prisma/client"
 
 //出荷ステータス
 export const SHIPPING_STATUS_OPTIONS = [
@@ -16,4 +16,11 @@ export const PRODUCT_TYPE_OPTIONS = [
   { label: "その他", value: ProductType.OTHER },
 ] as const satisfies readonly { label: string; value: ProductType }[]
 
-
+// 告示情報種別
+export const ANNOUNCE_TYPE_OPTIONS = [
+  { label: "通常出荷", value: AnnounceType.NORMAL_SHIPMENT },
+  { label: "限定出荷", value: AnnounceType.LIMITED_SHIPMENT },
+  { label: "出荷停止", value: AnnounceType.SHIPMENT_SUSPENDED },
+  { label: "販売中止", value: AnnounceType.DISCONTINUED_SALE },
+  { label: "販売移管", value: AnnounceType.TRANSFER_OF_SALE },
+] as const satisfies readonly { label: string; value: AnnounceType }[]
