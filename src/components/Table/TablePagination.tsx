61a7a8478347542d35bcd74e51a101a1aa807059
Ryo-k-113 <r.kameda.113@gmail.com>
2026-07-01 19:54:47 +0900

    refactor: TablePaginationを型安全に修正

--- src/components/Table/TablePagination.tsx
+++ src/components/Table/TablePagination.tsx
@@ -8,12 +8,27 @@ interface TablePaginationProps<TData> {
   table: Table<TData>
 }
 
-export const TablePagination = ({ table }: TablePaginationProps<TData>) => {
+export const TablePagination = <TData,>({ table }: TablePaginationProps<TData>) => {
 
   return (
     <div className="flex items-center justify-end space-x-2 py-4">
-      <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>前へ</Button>
-      <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>次へ</Button>
+      <Button 
+        variant="outline" 
+        size="sm" 
+        onClick={() => table.previousPage()} 
+        disabled={!table.getCanPreviousPage()}
+      >
+        前へ
+      </Button>
+
+      <Button 
+      variant="outline" 
+      size="sm" 
+      onClick={() => table.nextPage()} 
+      disabled={!table.getCanNextPage()}
+      >
+        次へ
+      </Button>
     </div>
   )
 }
