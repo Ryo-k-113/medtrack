6de9b9958e5beaeb973c13ae9005c52821f0f4bd
Ryo-k-113 <r.kameda.113@gmail.com>
2026-07-08 18:41:05 +0900

    feat: 型安全なHTTPメソッド(POST,PUT)を実装

---
+++ src/app/api/admin/units/[unitId]/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/prisma"
+import { adminAuthCheck } from "@/app/api/admin/_lib/adminAuthCheck"
+import type { UpdateUnitRequest, UpdateUnitResponse } from "@/types/admin/unit"
+
+
+/** 規格単位を更新 */
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { unitId: string } } 
+) => {
+
+  // 認証チェック
+  const { isAuthorized, error, status } = await adminAuthCheck(request)
+  if (!isAuthorized) return NextResponse.json({ error }, { status })
+  
+  // 規格単位IDの取得
+  const { unitId } = params;
+
+  try {
+    // リクエストbodyを取得
+    const body: UpdateUnitRequest = await request.json()
+    const { name } = body
+
+    // DBの規格単位を更新
+    const unit = await prisma.unit.update({
+      where: { id: parseInt(unitId) },
+      data: { name },
+    })
+
+    // 成功レスポンスを返す
+    return NextResponse.json<UpdateUnitResponse>(
+      { message: "更新しました", data: unit },
+      { status: 200 }
+    )
+  } catch {
+    return NextResponse.json(
+      { message: "データの更新中にエラーが発生しました" },
+      { status: 400 }
+    )
+  }
+}
--- src/app/api/admin/units/route.ts
+++ src/app/api/admin/units/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
-import type { GetUnitsResponse } from "@/types/admin/unit"
+import type { GetUnitsResponse, CreateUnitRequest, CreateUnitResponse } from "@/types/admin/unit"
 import { adminAuthCheck } from "../_lib/adminAuthCheck";
 
 
@@ -25,5 +25,35 @@ export const GET = async (request: NextRequest) => {
   } catch (error) {
     if (error instanceof Error)
       return NextResponse.json({ message: error.message }, { status: 400 })
+  }
+}
+
+
+/** 規格単位を新規作成 */
+export const POST = async (request: NextRequest) => {
+  // 認証チェック
+  const { isAuthorized, error, status } = await adminAuthCheck(request)
+  if (!isAuthorized) return NextResponse.json({ error }, { status })
+
+  try {
+    // リクエストbodyを取得
+    const body: CreateUnitRequest = await request.json()
+    const { name } = body
+    
+    // 規格単位をDBに作成
+    const unit = await prisma.unit.create({
+      data: { name },
+    })
+
+    // 成功レスポンスを返す
+    return NextResponse.json<CreateUnitResponse>(
+      { message: `${unit.name}を作成しました`, data: unit },
+      { status: 200 }
+    )
+  } catch {
+    return NextResponse.json(
+      { message: "データの作成中にエラーが発生しました" },
+      { status: 400 }
+    )
   }
 }
