067d8167bd1d4aac67ecb7a946d63e3d45a8b5fa
Ryo_k <r.kameda.113@gmail.com>
2026-07-20 15:41:38 +0900

    doc: fetcher関数にJSDocコメントを追加

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -5,6 +5,17 @@ type FetcherProps = {
   token?: string | null;
 };
 
+/**
+ * アプリケーション共通のAPIクライアント
+ *  @param url - APIエンドポイントパス
+ *  @param method? - HTTPメソッド (GET, POST, PUT, DELETE等)
+ *  @param body? - リクエストボディ（オブジェクトで渡すと自動でJSON.stringify）
+ *  @param token? - アクセストークン（認証が必要なAPI用）
+ *  @returns パース済みのJSONデータ
+ *  @throws {Error} - HTTPステータスが200以外は、APIが返したエラーメッセージを自動でパースして `throw`
+ **/
+
+
 export const fetcher = async ({
   url,
   method = "GET",
