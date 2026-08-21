31256aad0cf3e37831652c6bef9ece26eb3e514e
Ryo_k <r.kameda.113@gmail.com>
2026-08-21 10:13:24 +0900

    refactor: page.tsxのローディングコンポーネントを削除

--- src/app/(admin)/admin/drugs/[drugId]/page.tsx
+++ src/app/(admin)/admin/drugs/[drugId]/page.tsx
@@ -3,7 +3,6 @@ import * as React from "react"
 import { DrugEditForm } from "./_components/DrugEditForm"
 import { AdminPageTitle } from "../../_components/AdminPageTitle"
 import { useAdminDrug } from "./_hooks/useAdminDrug"
-import AdminDrugEditLoading from "./loading"
 import { notFound } from "next/navigation"
 import { PackageUnitListSection } from "./_components/PackageUnitListSection"
 
@@ -13,22 +12,20 @@ export default function AdminDrugEditPage() {
   // 製品情報を取得
   const { drug, isDrugLoading:isLoading } = useAdminDrug()
 
-  // ローディング画面
-  if (isLoading) return <AdminDrugEditLoading />
-  if (!drug) notFound()
+  // 医薬品がなければ notFound
+  if (!isLoading && !drug) notFound()
 
   return (
     <div>
       {/* タイトルと戻るボタン */}
       <AdminPageTitle
-        title={drug?.name}
+        title={drug?.name ?? ""}
         backTo="/admin/drugs"
         backButtonText="一覧へ戻る"
       />
-
       {/* 医薬品の編集フォーム */}
       <DrugEditForm />
-      
+
       {/* 包装一覧 */}
       <PackageUnitListSection />
     </div> 
